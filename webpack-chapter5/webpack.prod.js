079dd904de20c75ba9c1ce973a2f81c69b8ed7d9
panxiaoxiao0823 <anu0823@163.com>
2023-06-08 20:03:19 +0800

    增加第五章

---
+++ webpack-chapter5/webpack.prod.js
@@ -0,0 +1,212 @@
+"use strict";
+
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
+const webpack = require("webpack");
+const CleanWebpackPlugin = require("clean-webpack-plugin");
+const glob = require("glob");
+const HtmlWebpackExternalsPlugin = require("html-webpack-externals-plugin");
+// const FriendlyErrorsWebpackPlugin = require('friendly-errors-webpack-plugin');
+const SpeedMeasureWebpackPlugin = require('speed-measure-webpack-plugin'); // 分析构建速度插件
+
+const smp = new SpeedMeasureWebpackPlugin();
+
+// 设置多页面打包方案
+const setMPA = () => {
+  const entry = {};
+  const htmlWebpackPlugins = [];
+  const entryFiles = glob.sync(path.join(__dirname, "./src/views/*/index.js"));
+
+  Object.keys(entryFiles).map((index) => {
+    const entryFile = entryFiles[index];
+    // '/Users/cpselvis/my-project/src/index/index.js'
+
+    const match = entryFile.match(/src\/views\/(.*)\/index\.js/);
+    const pageName = match && match[1];
+
+    entry[pageName] = entryFile;
+    htmlWebpackPlugins.push(
+      new HtmlWebpackPlugin({
+        inlineSource: ".css$",
+        template: path.join(__dirname, `src/views/${pageName}/index.html`),
+        filename: `${pageName}.html`,
+        chunks: ["vendors", pageName],
+        inject: true,
+        minify: {
+          html5: true,
+          collapseWhitespace: true,
+          preserveLineBreaks: false,
+          minifyCSS: true,
+          minifyJS: true,
+          removeComments: false,
+        },
+      })
+    );
+  });
+
+  return {
+    entry,
+    htmlWebpackPlugins,
+  };
+};
+
+const { entry, htmlWebpackPlugins } = setMPA();
+
+module.exports = smp.wrap({
+  entry: entry,
+  output: {
+    path: path.join(__dirname, "dist"),
+    filename: "[name]_[chunkhash:8].js",
+  },
+  plugins: [
+    new MiniCssExtractPlugin({
+      filename: "[name]_[contenthash:8].css",
+    }),
+    new OptimizeCSSAssetsPlugin({
+      assetNameRegExp: /\.css$/g,
+      cssProcessor: require("cssnano"),
+    }),
+    // new HtmlWebpackPlugin({
+    //   title: "ceshi document title",
+    //   filename: "index.html",
+    //   template: path.join(__dirname, "src/index.html"),
+    // templateParameters: {
+    //   foo: "bar",
+    // },
+    //   chunks: ["app"],
+    //   inject: true,
+    //   base: "https://example.com/path/page.html",
+    //   minify: {
+    //     html5: true,
+    //     collapseWhitespace: true,
+    //     preserveLineBreaks: false,
+    //     minifyCSS: true,
+    //     minifyJS: true,
+    //     removeComments: false,
+    //   },
+    // }),
+    // new HtmlWebpackExternalsPlugin({
+    //   externals: [
+    //     {
+    //       module: 'react',
+    //       entry: 'https://cdn.staticfile.org/react/16.4.0/umd/react.development.js',
+    //       global: 'React',
+    //     },
+    //     {
+    //       module: 'react-dom',
+    //       entry: 'https://cdn.staticfile.org/react-dom/16.4.0/umd/react-dom.development.js',
+    //       global: 'ReactDOM',
+    //     },
+    //   ]
+    // }),
+    new webpack.optimize.ModuleConcatenationPlugin(),
+    new CleanWebpackPlugin(),
+    // new FriendlyErrorsWebpackPlugin(),
+  ].concat(htmlWebpackPlugins),
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+          // 'eslint-loader'
+        ]
+      },
+      {
+        test: /\.css$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [
+                require("autoprefixer")({
+                  browsers: ["last 2 version", ">1%", "ios 7"],
+                }),
+              ],
+            },
+          },
+          {
+            loader: "px2rem-loader",
+            options: {
+              remUnit: 75, // 1rem = 75px，则750px等于10rem，可根据设计稿灵活设置
+              remPrecision: 8, // px转换为rem单位时保留的小数位数
+            },
+          },
+        ],
+      },
+      {
+        test: /\.less$/,
+        exclude: /node_modules/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          "css-loader",
+          "less-loader",
+          {
+            loader: "postcss-loader",
+            options: {
+              plugins: () => [
+                require("autoprefixer")({
+                  browsers: ["last 2 version", ">1%", "ios 7"],
+                }),
+              ],
+            },
+          },
+          {
+            loader: "px2rem-loader",
+            options: {
+              remUnit: 75, // 1rem = 75px，则750px等于10rem，可根据设计稿灵活设置
+              remPrecision: 8, // px转换为rem单位时保留的小数位数
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(png|jpg|jpeg|gif)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name]_[hash:8].[ext]",
+            },
+          },
+        ],
+      },
+      {
+        test: /.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              name: "[name]_[hash:8].[ext]",
+            },
+          },
+        ],
+      },
+    ],
+  },
+  mode: "production",
+  devtool: "cheap-module-source-map",
+  // externals: {
+  //   react: 'React',
+  //   'react-dom': 'ReactDOM',
+  // },
+  optimization: {
+    splitChunks: {
+      minSize: 1000,
+      cacheGroups: {
+        commons: {
+          name: "commons",
+          chunks: "all",
+          minChunks: 2,
+        },
+      },
+    },
+  },
+  stats: 'errors-only'
+});
